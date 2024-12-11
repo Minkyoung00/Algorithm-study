c938834b4116a09c0335c2dc614cf60b03968301
kimkeongeun <118241290+kimkeongeun@users.noreply.github.com>
2024-12-11 11:33:32 +0900

    김경은_수

---
+++ 2024_12/11053_가장_긴_증가하는_부분_수열/김경은_11053bj.py
@@ -0,0 +1,14 @@
+N = int(input())
+nums = [*map(int,input().split())]
+stack = [nums[0]]
+
+for i in nums[1:]:
+    if stack[-1] < i:
+        stack.append(i)
+    else:
+        for j,v in enumerate(stack):
+            if i <= v:
+                stack[j] = i
+                break
+
+print(len(stack))
