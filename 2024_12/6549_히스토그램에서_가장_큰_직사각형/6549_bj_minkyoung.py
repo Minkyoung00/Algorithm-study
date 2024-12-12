1a81c319cc3cdb9f05d705ad4746115decb588d5
Minkyoung00 <kina12345600@gmail.com>
2024-12-12 12:30:43 +0900

    2024.12.12

--- 2024_12/6549_히스토그램에서_가장_큰_직사각형/6549_bj_minkyoung.py
+++ 2024_12/6549_히스토그램에서_가장_큰_직사각형/6549_bj_minkyoung.py
@@ -6,14 +6,34 @@ while True:
     hists.append(list(map(int, test.split())))
 
 for hist in hists:
-    n = hist.pop(0)
-    rectangles = [max(hist), n * min(hist)]
-    for i in range(2,n):  # 폭 
-        max_ = 0
-        for j in range(n-i+1):
-            max_ = max(max_, i * min(hist[j:j+i]))
-        
-        if max_ == i: break
-        rectangles.append(max_)
+    n = hist.pop(0)  
+    heights = hist + [0]  
+    stack = []
+    max_area = 0
 
-    print(max(rectangles))
+    for i in range(len(heights)):
+        h = heights[i]
+        while stack and heights[stack[-1]] > h:
+            height = heights[stack.pop()]
+            if stack:
+                width = i - stack[-1] - 1
+            else:
+                width = i
+            max_area = max(max_area, height * width)
+        stack.append(i)
+
+    print(max_area)
+
+# for hist in hists:
+#     n = hist.pop(0)
+#     rectangles = [max(hist), n * min(hist)]
+
+#     for i in range(2,n):  # 폭 
+#         max_ = 0
+#         for j in range(n-i+1):
+#             max_ = max(max_, i * min(hist[j:j+i]))
+        
+#         if max_ == i: break
+#         rectangles.append(max_)
+
+#     print(max(rectangles))
