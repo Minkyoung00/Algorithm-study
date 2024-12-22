a2249794c59b92a69efe38f5031ae03b613dd8ef
Minkyoung00 <kina12345600@gmail.com>
2024-12-22 17:48:17 +0900

    2024.12.19 ~ 2024.12.20

--- 2024_12/1932_정수_삼각형/1932_bj_minkyoung.py
+++ 2024_12/1932_정수_삼각형/1932_bj_minkyoung.py
@@ -5,16 +5,14 @@ tri = [tuple(map(int, input().split())) for i in range(N)]
 
 visit = [[0 for k in range(i)] for i in range(1,N+1)]
 visit[0][0] = tri[0][0]
-result = []
 
 for i in range(1, N):
-    row = i
     for j in range(i+1):
         if j == 0:
-            visit[row][j] = visit[row-1][j] + tri[row][j]
+            visit[i][j] = visit[i-1][j] + tri[i][j]
         elif j == i:
-            visit[row][j] = visit[row-1][j-1] + tri[row][j] 
+            visit[i][j] = visit[i-1][j-1] + tri[i][j] 
         else:
-            visit[row][j] = max(visit[row-1][j], visit[row-1][j-1]) + tri[i][j]
+            visit[i][j] = max(visit[i-1][j], visit[i-1][j-1]) + tri[i][j]
 
 print(max(visit[-1]))
