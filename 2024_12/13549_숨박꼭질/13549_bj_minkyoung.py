a0caef36b12652c418bbe152ac66941a6f3379c1
Minkyoung00 <kina12345600@gmail.com>
2024-12-23 14:12:06 +0900

    2024.12.23

--- 2024_12/13549_숨박꼭질/13549_bj_minkyoung.py
+++ 2024_12/13549_숨박꼭질/13549_bj_minkyoung.py
@@ -1,14 +1,31 @@
-N, K = (int(i) for i in input().split())
+from collections import deque
+
+N, K = map(int, input().split())
+
+result = []
+queue = deque()
 
 if N < K:
-    if K % 2 == 0:  # K 짝수
-        while N < K:
-            K //= 2
-        print(min(N-K, 2*K - N))
-    else:           # K 홀수
-        K -= 1
-        while N < K:
-            K //= 2
-        print(min(N-K, 2*K - N) + 1)
+    if N == 0 and K > 0:
+        N += 1
+        queue.append((K, 1)) 
+    else:
+        queue.append((K, 0)) 
 else:
-    print(N-K)
+    result.append(N-K)
+
+while queue:
+    K, count = queue.popleft()
+
+    while K % 2 == 0 and N < K:
+        K //= 2
+
+    if K % 2 != 0 and N < K:
+        queue.append((K + 1, count + 1))
+        queue.append((K - 1, count + 1))
+
+    if N >= K:
+        count += min(N-K, 2*K - N)
+        result.append(count)
+   
+print(min(result))
