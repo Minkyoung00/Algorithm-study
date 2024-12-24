0fd3c5d935b6a4edcd0010d559165ebaa098fbdc
Minkyoung00 <kina12345600@gmail.com>
2024-12-24 12:15:38 +0900

    2024.12.24(optimized)

--- 2024_12/4542_Blue_jeans/4542_bj_minkyoung.py
+++ 2024_12/4542_Blue_jeans/4542_bj_minkyoung.py
@@ -3,20 +3,21 @@ n = int(input())
 for i in range(n):
     m = int(input())
     first_sequence = input()
-    sequences = []
-    
-    for j in range(m-1):
-        sequences.append(input())
+    sequences = [input() for j in range(m-1)]
+    result = []
 
     for length in range(60, 2, -1):
-        result = []
+        check = set()
         
         for index in range(61 - length):
             cur = first_sequence[index : index+length]
+
+            if cur in check: continue
+            check.add(cur)
+
             count = 0
             for sequence in sequences:
-                p_cur = 0
-                p_seq = 0
+                p_cur = p_seq = 0
                 while p_seq != 60 and p_cur != length:
                     if sequence[p_seq] == cur[p_cur]:
                         p_seq += 1
