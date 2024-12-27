668a48e7945a1587316f1af405f65891b399ec6c
Minkyoung00 <kina12345600@gmail.com>
2024-12-27 14:05:50 +0900

    2024.12.27

--- 2024_12/2812_크게_만들기/2812_bj_minkyoung.py
+++ 2024_12/2812_크게_만들기/2812_bj_minkyoung.py
@@ -1,14 +1,14 @@
 N, K = map(int, input().split())
 num = [int(i) for i in input()]
-result = 0
-pre_index = -1
+result = []  
 
-for i in range(N-K):
-    max = 0
-    for j in range(pre_index + 1, N-K-1+i):
-        if max < num[j]:
-            max = num[j]
-            pre_index = j 
-    result += max * 10**(N-K-1-i)
+remove_count = K  
 
-print(result)
+for i in range(N):
+    while remove_count > 0 and result and result[-1] < num[i]:
+        result.pop()
+        remove_count -= 1
+    
+    result.append(num[i])
+
+print(''.join(map(str, result[:N-K])))
