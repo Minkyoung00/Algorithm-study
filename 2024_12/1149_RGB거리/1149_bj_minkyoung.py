ea4f8b38066911c7821e6a398f42dfdf6569e4b5
Minkyoung00 <kina12345600@gmail.com>
2024-12-31 12:20:28 +0900

    2024.12.31

--- 2024_12/1149_RGB거리/1149_bj_minkyoung.py
+++ 2024_12/1149_RGB거리/1149_bj_minkyoung.py
@@ -1,6 +1,9 @@
 N = int(input())
-cost = [map(int, input().split()) for i in range(N)]
-queue = []
+costs = [list(map(int, input().split())) for i in range(N)]
 
-for i in cost:
-    
+for i in range(1,N):
+    costs[i][0] = min(costs[i-1][1], costs[i-1][2]) + costs[i][0]
+    costs[i][1] = min(costs[i-1][0], costs[i-1][2]) + costs[i][1]
+    costs[i][2] = min(costs[i-1][0], costs[i-1][1]) + costs[i][2]
+
+print(min(costs[-1]))
