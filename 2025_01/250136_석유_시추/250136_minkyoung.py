d96d3ed1938f081b79027203e5ac5945d848cb5c
Minkyoung00 <kina12345600@gmail.com>
2025-01-06 12:45:43 +0900

    2025.01.06

--- 2025_01/250136_석유_시추/250136_minkyoung.py
+++ 2025_01/250136_석유_시추/250136_minkyoung.py
@@ -1,64 +1,45 @@
 def solution(land):
     n = len(land)       #세로
     m = len(land[0])    #가로
-
-    trans_land = list(zip(*land))
     
     moving = [(1,0),(-1,0),(0,1),(0,-1)]
     visited = [[-1] * m for i in range(n)]
 
     answer = 0
-    fuel = [0]*m
+    fuels = []
 
     for j in range(m):
-        if not trans_land[j]:
-            continue
         count = 0
-
+        drilled = []
         for i in range(n):
-            temp = []
-            if visited[i][j] >= 0 and (visited[i][j] not in temp):
-                count += fuel[visited[i][j]]
-                temp.append(visited[i][j])
+            # 방문을 했는데(확인된 석유덩어리인데) 시추되었다고 표시 안 됨
+            if visited[i][j] >= 0 and (visited[i][j] not in drilled):
+                count += fuels[visited[i][j]]
+                drilled.append(visited[i][j])
+
+            # 방문을 안 했는데 석유가 있는 곳
             if visited[i][j] < 0 and land[i][j]:
                 queue = [(i,j)]
-                visited[j][i] = j
+                visited[i][j] = len(fuels) # 어떤 석유 덩어리에 포함되는지 표시
 
+                # 새로운 석유 덩어리 조사사
+                size = 0
                 while queue:
                     cur_y, cur_x = queue.pop()
-                    count += 1 
+                    size += 1
                     for x, y in moving:
                         mov_y, mov_x = cur_y+y, cur_x+x
                         if 0 <= mov_x < m and 0 <= mov_y < n and land[mov_y][mov_x] and visited[mov_y][mov_x] < 0:
-                            visited[mov_y][mov_x] = j
+                            visited[mov_y][mov_x] = len(fuels)
                             queue.append((mov_y,mov_x))
 
-        fuel[j] = count
+                drilled.append(len(fuels))
+                fuels.append(size)
+                count += size
+
         answer = max(answer, count)
-    print(fuel)
 
     return answer
 
-input_v = 	[[1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 0, 0], [1, 0, 1, 0, 0, 1], [1, 0, 0, 1, 0, 0], [1, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1]]
-print((solution(input_v)))
-
-
-    # fuel = []
-        
-    # for i in range(n):
-    #     for j in range(m):
-    #         if not visited[i][j] and land[i][j]:
-    #             queue = [(i,j)]
-    #             count = 0
-    #             min_x, max_x = j, j
-    #             while queue:
-    #                 cur_y, cur_x = queue.pop()
-    #                 for x, y in moving:
-    #                     mov_y, mov_x = cur_y+y, cur_x+x
-    #                     if land[mov_y][mov_x] and not visited[mov_y][mov_x] and  0 <= mov_x <= m and 0 <= mov_y <= n:
-    #                         visited[mov_y][mov_x] = True
-    #                         min_x = min(min_x, mov_x)
-    #                         max_x = max(max_x, mov_x)
-    #                         queue.append((mov_y,mov_x))
-    #                         count += 1 
-    #             fuel.append(count, min_x, max_x)
+a = [[1, 0, 1, 0, 1, 1], [1, 0, 1, 0, 0, 0], [1, 0, 1, 0, 0, 1], [1, 0, 0, 1, 0, 0], [1, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1]]
+print(solution(a))
