e6dbdad8b888d05f7353eb424ab9945514acb1f1
Minkyoung00 <kina12345600@gmail.com>
2025-01-10 11:53:02 +0900

    2025.01.10

---
+++ 2025_01/13398_연속합2/13398_bj_minkyoung.py
@@ -0,0 +1,25 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+nagatives_idx = []
+nagatives_cnt = 0
+
+for i in range(n):
+    if nums[i] < 0:
+        nagatives_idx.append(i)
+        nagatives_cnt += 1
+
+    
+dp = [0] * (nagatives_cnt + 1)
+
+for i in range(nagatives_cnt+1):
+    if i  == 0:
+        dp[i] = sum(nums[:nagatives_idx[i]])
+        continue
+    if i == nagatives_cnt:
+        if nagatives_idx[i-1] < n-1:
+            dp[i] = sum(nums[nagatives_idx[i-1] + 1:])
+        continue
+    dp[i] = sum(nums[nagatives_idx[i-1]+1:nagatives_idx[i]])
+
+print(dp)
